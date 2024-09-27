7ffcbffcbacdbeffc9238c4e6893344acc1f47df
affbro1 <131714697+affbro1@users.noreply.github.com>
2024-09-27 14:14:41 -0600

    hm1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is the grade percent?");
+        string grade = Console.ReadLine(); 
+        int percent = int.Parse(grade);
+
+        if (percent >= 90){
+            Console.WriteLine("The entered grade is an A");}
+        else if (percent >=80){
+            Console.WriteLine("The entered grade is a B");
+        } 
+        else if (percent >=70){
+            Console.WriteLine("The entered grade is a C");
+        }
+        else if (percent >= 60){
+            Console.WriteLine("The entered grade is a D");
+        }
+        else Console.WriteLine("The enetered grade is an F");
     }
 }
