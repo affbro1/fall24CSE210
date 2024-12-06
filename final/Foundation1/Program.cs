670b2f255d57804adf539ecdb282d30da185980d
affbro1 <131714697+affbro1@users.noreply.github.com>
2024-12-05 17:55:34 -0700

    asdf

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        
+        List<Video> videos = new List<Video>();
+
+       
+        Video video1 = new Video("Introduction to C#", "John Foxworthy", 300);
+        video1.Comments.Add(new Comment("Alice", "Great tutorial!"));
+        video1.Comments.Add(new Comment("Bob", "Very informative, thanks!"));
+        videos.Add(video1);
+
+        Video video2 = new Video("Advanced C# Techniques", "Jane DeSante's", 450);
+        video2.Comments.Add(new Comment("Charlie", "This was a bit too advanced for me."));
+        video2.Comments.Add(new Comment("Dave", "Fantastic content, very clear!"));
+        videos.Add(video2);
+
+        Video video3 = new Video("C# Design Patterns", "Steve Jobs", 1600);
+        video3.Comments.Add(new Comment("Eve", "Love the design patterns explained here."));
+        videos.Add(video3);
+
+        Video video4 = new Video("C# and .NET Core", "Sarah Lee Harris", 700);
+        video4.Comments.Add(new Comment("Frank", "Very helpful, thanks!"));
+        video4.Comments.Add(new Comment("Grace", "I learned a lot."));
+        video4.Comments.Add(new Comment("Hank", "The best .NET Core tutorial!"));
+        videos.Add(video4);
+
+      
+        foreach (var video in videos)
+        {
+            video.DisplayVideoInfo();
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    public List<Comment> Comments { get; set; }
+
+   
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+        Comments = new List<Comment>();
+    }
+
+    
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+
+    
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {LengthInSeconds} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+
+
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"    {comment.Name} - {comment.Text}");
+        }
+        Console.WriteLine();  
+    }
+}
